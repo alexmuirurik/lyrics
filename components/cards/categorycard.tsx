6a38d328a259d715938847e82df5ea0596c112dd
Alex Muiruri <content@alexmuiruri.com>
2024-12-19 19:06:27 +0300

    Added templates to all the navigation

--- components/cards/categorycard.tsx
+++ components/cards/categorycard.tsx
@@ -8,12 +8,14 @@ import { Text, TouchableOpacity, View } from 'react-native';
 const Categorycard = ({ category }: { category: MusicCategory }) => {
     return (
         <TouchableOpacity
-            className='bg-[#1b2b60] flex-row items-center gap-3 border border-gray-400 rounded-md w-full p-5 mb-2'
+            className='bg-[#1b2b60] flex-row items-center gap-3 border border-gray-400 rounded-md w-full p-4 mb-2'
             onPress={() => router.navigate({ pathname: '/categories/[category]', params: { category: category.title } })}
         >
-            <FontAwesome name='folder-open' size={20} color={'white'} />
+            <View className='bg-[#1c2c5c] border border-gray-600 justify-center items-center rounded-xl w-12 h-12'>
+               <FontAwesome name='folder-open' size={20} color={'white'} /> 
+            </View>
             <View className='flex-row items-center gap-2'>
-                <Text className='text-gray-300 text-base font-semibold font-poppins'>
+                <Text className='text-gray-300 text-lg font-bold font-poppins'>
                     {ToTitleCase(category.title)}
                 </Text>
             </View>
